e4f01c7c7ac8a929860e1cb7f9434a259638c797
Shardul Khandekar <khandekarshardul@gmail.com>
2023-01-26 23:26:12 +0530

    Initial commit

---
+++ src/main/java/edu/mysqlspringboot/MysqlspringbootApplication.java
@@ -0,0 +1,13 @@
+package edu.mysqlspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MysqlspringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MysqlspringbootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/edu/mysqlspringboot/MysqlspringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.mysqlspringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MysqlspringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
